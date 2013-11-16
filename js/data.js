5c7cf01b9b2f4336e572d9efa569363f649ad21f
gardner <gbickfor@adobe.com>
2013-11-15 19:19:06 -0800

    Fixed js

--- js/data.js
+++ js/data.js
@@ -2,8 +2,7 @@ var providers = [
 	{"id":1,"name":"Fidelity investments","phone":"1 (800) 835-5097","SRI":"Gabelli SRI Fund"},
 	{"id":2,"name":"Vanguard","phone":"1 (877) 662-7447","SRI":"FTSE Social Index Fund"},
 	{"id":3,"name":"Merrill Lynch","phone":"1 (888) 654-6837","SRI":"Merrill Edge"},
-	{"id":4,"name":"ING","phone":"1 (800) 992-0180","SRI":"ESG Managers Portfolios","Holdings":"Top Holdings: Neuberger Berman Socially Responsive Fund, Parnassus Equity Income Fund, PAX World Small Cap Fund, Ariel Fund
-"},
+	{"id":4,"name":"ING","phone":"1 (800) 992-0180","SRI":"ESG Managers Portfolios","Holdings":"Top Holdings: Neuberger Berman Socially Responsive Fund, Parnassus Equity Income Fund, PAX World Small Cap Fund, Ariel Fund"},
 	{"id":5,"name":"Nationwide Financial","phone":"1 (877) 245-0761","SRI":"Nationwide Alternative Allocations Fund"},
 	{"id":6,"name":"Principal Financial Group","phone":"1 (800) 247-4695","SRI":""},
 	{"id":7,"name":"Citigroup","phone":"1 (800) 248-4465","SRI":""},
