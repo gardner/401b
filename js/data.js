4dbc800558f9c9b622b13343c062fa62df3504f7
gardner <gbickfor@adobe.com>
2013-11-15 16:07:43 -0800

    Adding initial files.

---
+++ js/data.js
@@ -0,0 +1,13 @@
+var providers = [
+	{"id":1,"name":"Fidelity investments","phone":"1 (800) 835-5097","SRI":"Gabelli SRI Fund"},
+	{"id":2,"name":"Vanguard","phone":"1 (877) 662-7447","SRI":"FTSE Social Index Fund"
+	{"id":3,"name":"Merrill Lynch","phone":"1 (888) 654-6837","SRI":"Merrill Edge"},
+	{"id":4,"name":"ING","phone":"1 (800) 992-0180","SRI":"ESG Managers Portfolios","Holdings":"Top Holdings: Neuberger Berman Socially Responsive Fund, Parnassus Equity Income Fund, PAX World Small Cap Fund, Ariel Fund
+"},
+	{"id":5,"name":"Nationwide Financial","phone":"1 (877) 245-0761","SRI":"Nationwide Alternative Allocations Fund"},
+	{"id":6,"name":"Principal Financial Group","phone":"1 (800) 247-4695","SRI":""},
+	{"id":7,"name":"Citigroup","phone":"1 (800) 248-4465","SRI":""},
+	{"id":8,"name":"Hewitt Associates","phone":"1 (800) 890-3200","SRI":""},
+	{"id":9,"name":"Great West Retirement Services","phone":"1 (800) 338-4015","SRI":""},
+	{"id":10,"name":"TIAA-CREF","phone":"1 (800) 842-2252","SRI":"Gabelli SRI Fund"},
+]
